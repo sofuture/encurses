c2a6585ec8ff728fd1f53533d0b763d39484fffc
jz <jeff.zellner@gmail.com>
2010-11-17 16:23:03 -0500

    etc

--- c_src/encurses.c
+++ c_src/encurses.c
@@ -4,12 +4,19 @@
 #include <ncurses.h>
 #include "erl_nif.h"
 
-static WINDOW *root;
+#define _MAXWINDOWS 64
+
+static WINDOW *slots[_MAXWINDOWS+1];
 
 /** function prototypes **/
 
-/* return helpers */
+/* NIF management */
 
+static int load(ErlNifEnv* env, void** priv_data, ERL_NIF_TERM load_data);
+
+/* internal helpers */
+
+static int find_free_window_slot();
 static ERL_NIF_TERM done(ErlNifEnv* env, int code);
 static ERL_NIF_TERM boolean(ErlNifEnv* env, int value);
 
@@ -88,7 +95,32 @@ static ERL_NIF_TERM e_keypad(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]
 
 static ERL_NIF_TERM e_getch(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
 
-/* implementations */
+/** implementations **/
+
+/* NIF management */
+
+static int
+load(ErlNifEnv* env, void** priv_data, ERL_NIF_TERM load_info)
+{
+    int i;
+    for(i=0;i<_MAXWINDOWS;i++){
+        slots[i] = NULL;
+    }
+    return 0;
+}
+
+/* internal helper functions */
+
+static int
+find_free_window_slot()
+{
+    int i;
+    for(i=1;i<_MAXWINDOWS;i++){
+        if(slots[i] == NULL)
+            return i;
+    }
+    return -1;
+}
 
 static ERL_NIF_TERM 
 done(ErlNifEnv* env, int code)
@@ -126,6 +158,51 @@ e_refresh(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
     return done(env, refresh());
 }
 
+static ERL_NIF_TERM 
+e_wrefresh(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    long win;
+    enif_get_long(env, argv[0], &win);
+    return done(env, wrefresh(slots[win]));
+}
+
+// newwin
+
+static ERL_NIF_TERM 
+e_newwin(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    int slot = find_free_window_slot();
+    long width, height, startx, starty;
+    enif_get_long(env, argv[0], &width);
+    enif_get_long(env, argv[1], &height);
+    enif_get_long(env, argv[2], &startx);
+    enif_get_long(env, argv[3], &starty);
+    slots[slot] = newwin(height, width, starty, startx);
+    return enif_make_long(env, slot);
+}
+
+// delwin
+
+static ERL_NIF_TERM 
+e_delwin(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    long slot;
+    enif_get_long(env, argv[0], &slot);
+    if(slot == 0) 
+    {
+        return done(env, FALSE);
+    }
+    else if(slots[slot] == NULL) 
+    {
+        return done(env, FALSE);
+    }
+    else 
+    {
+        delwin(slots[slot]);
+        return done(env, OK);
+    }
+}
+
 // endwin
 
 static ERL_NIF_TERM 
@@ -139,8 +216,8 @@ e_endwin(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 static ERL_NIF_TERM 
 e_initscr(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
-    root = (WINDOW *)initscr();
-    return enif_make_long(env, (long)root);
+    slots[0] = (WINDOW *)initscr();
+    return enif_make_long(env, 0);
 }
 
 // cbreak
@@ -186,7 +263,7 @@ int_erase(ErlNifEnv* env, WINDOW *win)
 static ERL_NIF_TERM 
 e_erase(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
-    return int_erase(env, root);
+    return int_erase(env, slots[0]);
 }
 
 static ERL_NIF_TERM 
@@ -194,7 +271,7 @@ e_werase(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
     long win;
     enif_get_long(env, argv[0], &win);
-    return int_erase(env, (WINDOW *)win);
+    return int_erase(env, slots[win]);
 }
 
 // addch
@@ -207,6 +284,36 @@ e_addch(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
     return done(env, addch((chtype)ch));
 }
 
+static ERL_NIF_TERM 
+e_waddch(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    long win, ch;
+    enif_get_long(env, argv[0], &win);
+    enif_get_long(env, argv[1], &ch);
+    return done(env, waddch(slots[win], (chtype)ch));
+}
+
+static ERL_NIF_TERM 
+e_mvaddch(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    long x, y, ch;
+    enif_get_long(env, argv[0], &x);
+    enif_get_long(env, argv[1], &y);
+    enif_get_long(env, argv[2], &ch);
+    return done(env, mvaddch((int)y, (int)x, (chtype)ch));
+}
+
+static ERL_NIF_TERM 
+e_mvwaddch(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    long win, x, y, ch;
+    enif_get_long(env, argv[0], &win);
+    enif_get_long(env, argv[1], &x);
+    enif_get_long(env, argv[2], &y);
+    enif_get_long(env, argv[3], &ch);
+    return done(env, mvwaddch(slots[win], (int)y, (int)x, (chtype)ch));
+}
+
 // addstr
 
 static ERL_NIF_TERM
@@ -216,7 +323,51 @@ e_addstr(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
     enif_get_long(env, argv[0], &length);
     char buff[length];
     enif_get_string(env, argv[1], buff, length+1, ERL_NIF_LATIN1);
+
     return done(env, addnstr(buff, length));
+}
+
+static ERL_NIF_TERM 
+e_waddstr(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    long length;
+    enif_get_long(env, argv[1], &length);
+    char buff[length];
+    enif_get_string(env, argv[2], buff, length+1, ERL_NIF_LATIN1);
+
+    long win;
+    enif_get_long(env, argv[0], &win);
+
+    return done(env, waddnstr(slots[win], buff, length));
+}
+
+static ERL_NIF_TERM 
+e_mvaddstr(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    long length;
+    enif_get_long(env, argv[2], &length);
+    char buff[length];
+    enif_get_string(env, argv[3], buff, length+1, ERL_NIF_LATIN1);
+
+    long x, y;
+    enif_get_long(env, argv[0], &x);
+    enif_get_long(env, argv[1], &y);
+    return done(env, mvaddnstr((int)y, (int)x, buff, length));
+}
+
+static ERL_NIF_TERM 
+e_mvwaddstr(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    long length;
+    enif_get_long(env, argv[0], &length);
+    char buff[length];
+    enif_get_string(env, argv[1], buff, length+1, ERL_NIF_LATIN1);
+
+    long win, x, y;
+    enif_get_long(env, argv[0], &win);
+    enif_get_long(env, argv[1], &x);
+    enif_get_long(env, argv[2], &y);
+    return done(env, mvwaddnstr(slots[win], (int)y, (int)x, buff, length));
 }
 
 // move
@@ -228,6 +379,16 @@ e_move(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
     enif_get_long(env, argv[0], &x);
     enif_get_long(env, argv[1], &y);
     return done(env, move((int)y, (int)x));
+}
+
+static ERL_NIF_TERM
+e_wmove(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    long win, x, y;
+    enif_get_long(env, argv[0], &win);
+    enif_get_long(env, argv[1], &x);
+    enif_get_long(env, argv[2], &y);
+    return done(env, wmove(slots[win], (int)y, (int)x));
 }
 
 // getxy
@@ -245,7 +406,7 @@ int_getxy(ErlNifEnv* env, WINDOW *win)
 static ERL_NIF_TERM
 e_getxy(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
-    return int_getxy(env, root);
+    return int_getxy(env, slots[0]);
 }
 
 static ERL_NIF_TERM
@@ -253,7 +414,7 @@ e_wgetxy(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
     long win;
     enif_get_long(env, argv[0], &win);
-    return int_getxy(env, (WINDOW *)win);
+    return int_getxy(env, slots[win]);
 }
 
 // getmaxxy
@@ -271,7 +432,7 @@ int_getmaxxy(ErlNifEnv* env, WINDOW *win)
 static ERL_NIF_TERM
 e_getmaxxy(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
-    return int_getmaxxy(env, root);
+    return int_getmaxxy(env, slots[0]);
 }
 
 static ERL_NIF_TERM 
@@ -279,7 +440,7 @@ e_wgetmaxxy(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
     long win;
     enif_get_long(env, argv[0], &win);
-    return int_getmaxxy(env, (WINDOW *)win);
+    return int_getmaxxy(env, slots[win]);
 }
 
 // curs_set
@@ -308,13 +469,194 @@ e_start_color(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
     return done(env, start_color());
 }
 
+// init_pair
+
+static ERL_NIF_TERM
+e_init_pair(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    long n, fcolor, bcolor;
+    enif_get_long(env, argv[0], &n);
+    enif_get_long(env, argv[1], &fcolor);
+    enif_get_long(env, argv[2], &bcolor);
+    return done(env, init_pair((int)n, (int)fcolor, (int)bcolor));
+}
+
+// attron/attroff
+
+static ERL_NIF_TERM 
+e_attron(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    long attrs;
+    enif_get_long(env, argv[0], &attrs);
+    return done(env, wattron(slots[0], (int)attrs));
+}
+
+static ERL_NIF_TERM 
+e_wattron(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    long win, attrs;
+    enif_get_long(env, argv[0], &win);
+    enif_get_long(env, argv[1], &attrs);
+    return done(env, wattron(slots[win], (int)attrs));
+}
+
+static ERL_NIF_TERM 
+e_attroff(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    long attrs;
+    enif_get_long(env, argv[0], &attrs);
+    return done(env, wattroff(slots[0], (int)attrs));
+}
+
+static ERL_NIF_TERM 
+e_wattroff(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    long win, attrs;
+    enif_get_long(env, argv[0], &win);
+    enif_get_long(env, argv[1], &attrs);
+    return done(env, wattroff(slots[win], (int)attrs));
+}
+
+// nl/nonl
+
+static ERL_NIF_TERM 
+e_nl(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    return done(env, nl());
+}
+
+static ERL_NIF_TERM 
+e_nonl(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    return done(env, nonl());
+}
+
+// scrollok
+
+static ERL_NIF_TERM 
+e_scrollok(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    long win, flag;
+    enif_get_long(env, argv[0], &win);
+    enif_get_long(env, argv[1], &flag);
+    return done(env, scrollok(slots[win], (int)flag));
+}
+
+// vline/hline
+
+static ERL_NIF_TERM 
+e_hline(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    long ch, max;
+    enif_get_long(env, argv[0], &ch);
+    enif_get_long(env, argv[1], &max);
+    return done(env, whline(slots[0], (chtype)ch, (int)max));
+}
+
+static ERL_NIF_TERM 
+e_whline(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    long win, ch, max;
+    enif_get_long(env, argv[0], &win);
+    enif_get_long(env, argv[1], &ch);
+    enif_get_long(env, argv[2], &max);
+    return done(env, whline(slots[win], (chtype)ch, (int)max));
+}
+
+static ERL_NIF_TERM 
+e_vline(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    long ch, max;
+    enif_get_long(env, argv[0], &ch);
+    enif_get_long(env, argv[1], &max);
+    return done(env, wvline(slots[0], (chtype)ch, (int)max));
+}
+
+static ERL_NIF_TERM 
+e_wvline(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    long win, ch, max;
+    enif_get_long(env, argv[0], &win);
+    enif_get_long(env, argv[1], &ch);
+    enif_get_long(env, argv[2], &max);
+    return done(env, wvline(slots[win], (chtype)ch, (int)max));
+}
+
+// border
+
+static ERL_NIF_TERM 
+e_border(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    long ls, rs, ts, bs, tl, tr, bl, br;
+    enif_get_long(env, argv[0], &ls);
+    enif_get_long(env, argv[1], &rs);
+    enif_get_long(env, argv[2], &ts);
+    enif_get_long(env, argv[3], &bs);
+    enif_get_long(env, argv[4], &tl);
+    enif_get_long(env, argv[5], &tr);
+    enif_get_long(env, argv[6], &bl);
+    enif_get_long(env, argv[7], &br);
+    return done(env, wborder(slots[0], (chtype)ls, (chtype)rs, (chtype)ts,
+            (chtype)bs, (chtype)tl, (chtype)tr, (chtype)bl, (chtype)br));
+}
+
+static ERL_NIF_TERM 
+e_wborder(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    long win, ls, rs, ts, bs, tl, tr, bl, br;
+    enif_get_long(env, argv[0], &win);
+    enif_get_long(env, argv[1], &ls);
+    enif_get_long(env, argv[2], &rs);
+    enif_get_long(env, argv[3], &ts);
+    enif_get_long(env, argv[4], &bs);
+    enif_get_long(env, argv[5], &tl);
+    enif_get_long(env, argv[6], &tr);
+    enif_get_long(env, argv[7], &bl);
+    enif_get_long(env, argv[8], &br);
+    return done(env, wborder(slots[win], (chtype)ls, (chtype)rs, (chtype)ts,
+            (chtype)bs, (chtype)tl, (chtype)tr, (chtype)bl, (chtype)br));
+}
+
+// box
+
+static ERL_NIF_TERM 
+e_box(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    long win, hch, vch;
+    enif_get_long(env, argv[0], &win);
+    enif_get_long(env, argv[1], &hch);
+    enif_get_long(env, argv[2], &vch);
+    return done(env, box(slots[win], (chtype)vch, (chtype)hch));
+}
+
+// keypad
+
+static ERL_NIF_TERM 
+e_keypad(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    long win, flag;
+    enif_get_long(env, argv[0], &win);
+    enif_get_long(env, argv[1], &flag);
+    return done(env, keypad(slots[win], (int)flag));
+}
+
+// getch
+
+static ERL_NIF_TERM 
+e_getch(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    return done(env, OK);
+}
+
+/* NIF map */
+
 static ErlNifFunc nif_funcs[] =
 {
     {"e_refresh", 0, e_refresh},
     {"e_wrefresh", 1, e_wrefresh},
 
-    {"e_newwin", 4, e_endwin},
-    {"e_delwin", 1, e_endwin},
+    {"e_newwin", 4, e_newwin},
+    {"e_delwin", 1, e_delwin},
     {"e_endwin", 0, e_endwin},
 
     {"e_initscr", 0, e_initscr},
@@ -326,7 +668,7 @@ static ErlNifFunc nif_funcs[] =
     {"e_noecho", 0, e_noecho},
 
     {"e_erase", 0, e_erase},
-    {"e_erase", 1, e_werase},
+    {"e_werase", 1, e_werase},
 
     {"e_addch", 1, e_addch},
     {"e_waddch", 2, e_waddch},
@@ -334,17 +676,17 @@ static ErlNifFunc nif_funcs[] =
     {"e_mvwaddch", 4, e_mvwaddch},
 
     {"e_addstr", 2, e_addstr},
-    {"e_waddstr", 3, e_addstr},
-    {"e_mvaddstr", 4, e_addstr},
-    {"e_waddstr", 5, e_addstr},
+    {"e_waddstr", 3, e_waddstr},
+    {"e_mvaddstr", 4, e_mvaddstr},
+    {"e_mvwaddstr", 5, e_mvwaddstr},
 
     {"e_move", 2, e_move},
     {"e_wmove", 3, e_wmove},
 
     {"e_getxy", 0, e_getxy},
-    {"e_getxy", 1, e_wgetxy},
+    {"e_wgetxy", 1, e_wgetxy},
     {"e_getmaxxy", 0, e_getmaxxy},
-    {"e_getmaxxy", 1, e_wgetmaxxy},
+    {"e_wgetmaxxy", 1, e_wgetmaxxy},
 
     {"e_curs_set", 1, e_curs_set},
 
@@ -377,4 +719,4 @@ static ErlNifFunc nif_funcs[] =
     {"e_getch", 0, e_getch},
 };
 
-ERL_NIF_INIT(encurses, nif_funcs, NULL, NULL, NULL, NULL)
+ERL_NIF_INIT(encurses, nif_funcs, load, NULL, NULL, NULL)
--- src/encurses.erl
+++ src/encurses.erl
@@ -7,23 +7,50 @@
 -export([
         addch/1, 
         addstr/1,
+        attroff/1,
+        attroff/2,
+        attron/1,
+        attron/2,
+        border/8,
+        border/9,
+        box/3,
         cbreak/0, 
         curs_set/1,
+        delwin/1,
         echo/0,
         endwin/0,
         erase/0,
         erase/1,
+        getch/0,
         getxy/0,
         getxy/1,
         getmaxxy/0,
         getmaxxy/1,
+        hline/2,
+        hline/3,
         has_colors/0,
+        init_pair/3,
         initscr/0, 
+        keypad/2,
         move/2,
-        nocbreak/0, 
+        move/3,
+        mvaddch/3,
+        mvaddstr/3,
+        mvwaddch/4,
+        mvwaddstr/4,
+        newwin/4,
+        nl/0,
+        nocbreak/0,
         noecho/0, 
+        nonl/0,
         refresh/0,
-        start_color/0
+        refresh/1,
+        scrollok/2,
+        start_color/0,
+        vline/2,
+        vline/3,
+        waddch/2,
+        waddstr/2
     ]).
 
 %% =============================================================================
@@ -37,14 +64,33 @@ load_nif() ->
 %% Application API
 %% =============================================================================
 
+%% refresh
+
 refresh() ->
     e_refresh().
+
+refresh(Win) when is_integer(Win) ->
+    e_wrefresh(Win).
+
+%% window management
+
+newwin(Width, Height, StartX, StartY) when is_integer(Width) andalso 
+        is_integer(Height) andalso is_integer(StartX) andalso 
+        is_integer(StartY) ->
+    e_newwin(Width, Height, StartX, StartY).
+
+delwin(Win) when is_integer(Win) ->
+    e_delwin(Win).
 
 endwin() ->
     e_endwin().
 
+%% initscr
+
 initscr() ->
     e_initscr().
+
+%% cbreak
 
 cbreak() -> 
     e_cbreak().
@@ -52,54 +98,196 @@ cbreak() ->
 nocbreak() ->
     e_nocbreak().
 
+%% echo
+
 echo() ->
     e_echo().
 
 noecho() ->
     e_noecho().
 
-addch(Char) when is_integer(Char) ->
-    e_addch(Char).
+%% erase
 
 erase() ->
     e_erase().
 
 erase(Win) when is_integer(Win) ->
-    e_erase(Win).
+    e_werase(Win).
+
+%% addch
+
+addch(Char) when is_integer(Char) ->
+    e_addch(Char).
+
+waddch(Win, Char) when is_integer(Win) andalso is_integer(Char) ->
+    e_waddch(Win, Char).
+
+mvaddch(X, Y, Char) when is_integer(X) andalso is_integer(Y) andalso
+        is_integer(Char) ->
+    e_mvaddch(X, Y, Char).
+
+mvwaddch(Win, X, Y, Char) when is_integer(Win) andalso is_integer(X) andalso
+        is_integer(Y) andalso is_integer(Char) ->
+    e_mvwaddch(Win, X, Y, Char).
+
+%% addstr
 
 addstr(String) when is_list(String) ->
     Str = lists:flatten(String),
     e_addstr(erlang:iolist_size(Str), Str).
 
+waddstr(Win, String) when is_integer(Win) andalso is_list(String) ->
+    Str = lists:flatten(String),
+    e_waddstr(Win, erlang:iolist_size(Str), Str).
+
+mvaddstr(X, Y, String) when is_integer(X) andalso is_integer(Y) andalso
+        is_list(String) ->
+    Str = lists:flatten(String),
+    e_mvaddstr(X, Y, erlang:iolist_size(Str), Str).
+
+mvwaddstr(Win, X, Y, String) when is_integer(Win) andalso is_integer(X) andalso
+        is_integer(Y) andalso is_list(String) ->
+    Str = lists:flatten(String),
+    e_mvwaddstr(Win, X, Y, erlang:iolist_size(Str), Str).
+
+%% move
+
 move(X, Y) when is_integer(X) andalso is_integer(Y) ->
     e_move(X, Y).
+
+move(Win, X, Y) when is_integer(Win) andalso is_integer(X) andalso
+        is_integer(Y) ->
+    e_wmove(Win, X, Y).
+
+%% get x and y
 
 getxy() ->
     e_getxy().
 
 getxy(Win) when is_integer(Win) ->
-    e_getxy(Win).
+    e_wgetxy(Win).
 
 getmaxxy() ->
     e_getmaxxy().
 
 getmaxxy(Win) when is_integer(Win) ->
-    e_getmaxxy(Win).
+    e_wgetmaxxy(Win).
+
+%% curs_set
 
 curs_set(Flag) when is_integer(Flag) ->
     e_curs_set(Flag).
 
+%% has_colors
+
 has_colors() ->
     e_has_colors().
 
+%% start_color
+
 start_color() ->
     e_start_color().
+
+%% init_pair
+
+init_pair(N, FColor, BColor) when is_integer(N) andalso is_integer(FColor)
+        andalso is_integer(BColor) ->
+    e_init_pair(N, FColor, BColor).
+
+%% attron/attroff
+
+attron(Mask) when is_integer(Mask) ->
+    e_attron(Mask).
+
+attron(Win, Mask) when is_integer(Win) andalso is_integer(Mask) ->
+    e_wattron(Win, Mask).
+
+attroff(Mask) when is_integer(Mask) ->
+    e_attroff(Mask).
+
+attroff(Win, Mask) when is_integer(Win) andalso is_integer(Mask) ->
+    e_wattroff(Win, Mask).
+
+%% nl/nonl
+
+nl() ->
+    e_nl().
+
+nonl() ->
+    e_nonl().
+
+%% scrollok
+
+scrollok(Win, Flag) when is_integer(Win) andalso is_boolean(Flag) ->
+    case Flag of
+        true -> e_scrollok(Win, 1);
+        false -> e_scrollok(Win, 0)
+    end.
+
+%% hline/vline
+
+hline(Char, MaxN) when is_integer(Char) andalso is_integer(MaxN) ->
+    e_hline(Char, MaxN).
+
+hline(Win, Char, MaxN) when is_integer(Win) andalso is_integer(Char) 
+        andalso is_integer(MaxN) ->
+    e_whline(Win, Char, MaxN).
+
+vline(Char, MaxN) when is_integer(Char) andalso is_integer(MaxN) ->
+    e_vline(Char, MaxN).
+
+vline(Win, Char, MaxN) when is_integer(Win) andalso is_integer(Char)
+        andalso is_integer(MaxN) ->
+    e_wvline(Win, Char, MaxN).
+
+%% border
+
+border(Ls, Rs, Ts, Bs, TLs, TRs, BLs, BRs) 
+  when is_integer(Ls) andalso is_integer(Rs) andalso 
+        is_integer(Ts) andalso is_integer(Bs) andalso is_integer(TLs) andalso
+        is_integer(TRs) andalso is_integer(BLs) andalso is_integer(BRs) ->
+    e_border(Ls, Rs, Ts, Bs, TLs, TRs, BLs, BRs).
+
+border(Win, Ls, Rs, Ts, Bs, TLs, TRs, BLs, BRs) 
+  when is_integer(Win) andalso is_integer(Ls) andalso 
+        is_integer(Rs) andalso is_integer(Ts) andalso is_integer(Bs) andalso 
+        is_integer(TLs) andalso is_integer(TRs) andalso is_integer(BLs) andalso
+        is_integer(BRs) ->
+    e_wborder(Win, Ls, Rs, Ts, Bs, TLs, TRs, BLs, BRs).
+
+%% box
+
+box(Win, Horz, Vert) when is_integer(Win) andalso is_integer(Horz) andalso
+        is_integer(Vert) ->
+    e_box(Win, Horz, Vert).
+
+%% keypad
+
+keypad(Win, Flag) when is_integer(Win) and is_boolean(Flag) ->
+    case Flag of
+        true -> e_keypad(Win, 1);
+        false -> e_keypad(Win, 0)
+    end.
+
+%% getch
+
+getch() ->
+    e_getch().
 
 %% =============================================================================
 %% Internal functions
 %% =============================================================================
 
 e_refresh() ->
+    not_initialized.
+
+e_wrefresh(_Win) ->
+    not_initialized.
+
+e_newwin(_Width, _Height, _StartX, _StartY) ->
+    not_initialized.
+
+e_delwin(_Win) ->
     not_initialized.
 
 e_endwin() ->
@@ -120,34 +308,55 @@ e_echo() ->
 e_noecho() -> 
     not_initialized.
 
-e_addch(Char) when is_integer(Char) ->
-    not_initialized.
-
 e_erase() ->
     not_initialized.
 
-e_erase(Win) when is_integer(Win) ->
+e_werase(_Win) ->
     not_initialized.
 
-e_addstr(StrLen, String) when is_integer(StrLen) andalso is_list(String) ->
+e_addch(_Char) ->
     not_initialized.
 
-e_move(X, Y) when is_integer(X) andalso is_integer(Y) ->
+e_waddch(_Win, _Char) ->
+    not_initialized.
+
+e_mvaddch(_X, _Y, _Char) ->
+    not_initialized.
+
+e_mvwaddch(_Win, _X, _Y, _Char) ->
+    not_initialized.
+
+e_addstr(_StrLen, _String) ->
+    not_initialized.
+
+e_waddstr(_Win, _StrLen, _String) ->
+    not_initialized.
+
+e_mvaddstr(_X, _Y, _StrLen, _String) ->
+    not_initialized.
+
+e_mvwaddstr(_Win, _X, _Y, _StrLen, _String) ->
+    not_initialized.
+
+e_move(_X, _Y) ->
+    not_initialized.
+
+e_wmove(_Win, _X, _Y) ->
     not_initialized.
 
 e_getxy() ->
     not_initialized.
 
-e_getxy(Win) when is_integer(Win) ->
+e_wgetxy(_Win) ->
     not_initialized.
 
 e_getmaxxy() ->
     not_initialized.
 
-e_getmaxxy(Win) when is_integer(Win) ->
+e_wgetmaxxy(_Win) ->
     not_initialized.
 
-e_curs_set(Flag) when is_integer(Flag) ->
+e_curs_set(_Flag) ->
     not_initialized.
 
 e_has_colors() ->
@@ -155,3 +364,55 @@ e_has_colors() ->
 
 e_start_color() ->
     not_initialized.
+
+e_init_pair(_N, _FColor, _BColor) ->
+    not_initialized.
+
+e_attron(_Mask) ->
+    not_initialized.
+
+e_wattron(_Win, _Mask) ->
+    not_initialized.
+
+e_attroff(_Mask) ->
+    not_initialized.
+
+e_wattroff(_Win, _Mask) ->
+    not_initialized.
+
+e_nl() ->
+    not_initialized.
+
+e_nonl() ->
+    not_initialized.
+
+e_scrollok(_Win, _Flag) ->
+    not_initialized.
+
+e_hline(_Char, _MaxN) ->
+    not_initialized.
+
+e_whline(_Win, _Char, _MaxN) ->
+    not_initialized.
+
+e_vline(_Char, _MaxN) ->
+    not_initialized.
+
+e_wvline(_Win, _Char, _MaxN) ->
+    not_initialized.
+
+e_border(_Ls, _Rs, _Ts, _Bs, _TLs, _TRs, _BLs, _BRs) ->
+    not_initialized.
+
+e_wborder(_Win, _Ls, _Rs, _Ts, _Bs, _TLs, _TRs, _BLs, _BRs) ->
+    not_initialized.
+
+e_box(_Win, _Horz, _Vert) ->
+    not_initialized.
+
+e_keypad(_Win, _Flag) ->
+    not_initialized.
+
+e_getch() ->
+    not_initialized.
+
